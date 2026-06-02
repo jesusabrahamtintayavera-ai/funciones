f716c810766166fccfbbd8962bdef7dad6e7c76d
jesusabrahamtintayavera-ai <jesusabrahamtintayavera@gmail.com>
2026-06-02 11:11:55 -0400

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,10 @@
+#funcion sin parametros ni retorno
+def mostrar_fecha():
+    print("Hoy es un gran día para aprender Python")
+
+mostrar_fecha()
+#funcion con parametros y sin retorno
+def mostrar_calificacion(nombre, nota):
+    print(nombre, "obtuvo una calificación de", nota)
+
+mostrar_calificacion("María", 95)
